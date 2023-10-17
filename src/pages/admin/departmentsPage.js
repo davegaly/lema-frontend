97a78f132ba63bb87f6c10b1c9f030926e3d622e
davegaly <davegaly@gmail.com>
2023-10-17 18:00:45 +0200

    work on dept page

--- src/pages/admin/departmentsPage.js
+++ src/pages/admin/departmentsPage.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 const DepartmentsPage = () => {
 
-    const [users, setUsers] = useState([])
+    const [departmentsList, setDepartmentsList] = useState([])
+    const [departmentEdit, setDepartmentEdit] = useState({})
 
-    const fetchData = () => {
-      fetch("https://swift-fragrant-deer.glitch.me/departments/list")
+    const getList = async () => {
+      const response = await axios.get("https://swift-fragrant-deer.glitch.me/departments/list");
+      setDepartmentsList(response.data);
+    }
+
+    const getSingleDepartment = () => {
+      fetch("https://swift-fragrant-deer.glitch.me/departments/getbyid/")
         .then(response => {
           return response.json()
         })
@@ -13,23 +20,28 @@ const DepartmentsPage = () => {
           setUsers(data)
         })
     }
-  
-    useEffect(() => {
-      fetchData()
-    }, [])
 
+    const saveDepartment = async () => {
+      const response = await axios.post("https://swift-fragrant-deer.glitch.me/departments/update",{id:1, name:'ciao'});
+      console.log(response.data);
+    }
+  
+    useEffect(() => {getList()}, [])
 
     return (
         <div>
             <h1>Departments management</h1>
             <div>
-            {users.length > 0 && (
+            {departmentsList.length > 0 && (
                 <ul>
-                {users.map(user => (
-                    <li key={user.id}>{user.name}</li>
+                {departmentsList.map(department => (
+                    <li key={department.id}>{department.name}</li>
                 ))}
                 </ul>
             )}
+            </div>
+            <div>
+              <input type="text" ></input>
             </div>
         </div>
     )
